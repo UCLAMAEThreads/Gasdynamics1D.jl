837da52b05ac124651f7b66c2af7426ddb88a947
Jeff Eldredge <jdeldre@g.ucla.edu>
2021-03-27 17:04:20 -0700

    Added basic infrastructure for Unitful in thermo quantities

---
+++ src/utils.jl
@@ -0,0 +1,55 @@
+# Macros for creating units and quantities
+
+"""
+    @displayedunits(qty,a)
+
+Set the preferred units for displaying quantities and
+create function `displayedunits` for returning these units and
+`ushow` for converting quantities into these units.
+"""
+macro displayedunits(qty,a,dims)
+  utype = isdefined(Unitful, qty) ? getfield(Unitful,qty) : qty
+  esc(quote
+      @derived_dimension($qty,$dims)
+
+      displayedunits(::Type{$utype}) = @u_str($a)
+
+       function ushow(x::$utype)
+            return uconvert(@u_str($a),x)
+        end
+
+      end)
+end
+
+macro create_dimvar(qty,utype)
+  esc(quote
+          struct $qty{U<:$utype} <: ThermodynamicQuantity{U}
+            val :: U
+            name :: String
+          end
+          $qty(x::U) where {U<:$utype} = $qty(ushow(x),string($qty))
+          $qty(x::Real) = $qty(x*displayedunits($utype),string($qty))
+
+          export $qty
+      end)
+end
+
+macro create_nondimvar(qty)
+  esc(quote
+          struct $qty{U<:Real} <: ThermodynamicQuantity{U}
+            val :: U
+            name :: String
+          end
+          $qty(x::Real) = SpecificHeatRatio(x,string($qty))
+
+          export $qty
+      end)
+end
+
+macro create_gas(name,gamma,molarmass)
+    esc(quote
+            const $name = PerfectGas(γ = SpecificHeatRatio($gamma),
+                                     R = GasConstant(Unitful.R/$molarmass))
+            export $name
+        end)
+end
