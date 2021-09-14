39dd0ce7862b28271b74611f36b3362734c51abf
Jeff Eldredge <jdeldre@g.ucla.edu>
2021-09-14 15:59:51 -0700

    Added a setting for GKSwstype to run in headless form

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,9 @@ using Literate
 
 const GROUP = get(ENV, "GROUP", "All")
 
+#ENV["GKSwstype"] = "nul" # removes GKS warnings during plotting
+ENV["GKSwstype"] = "100"
+
 
 notebookdir = "../notebook"
 docdir = "../docs/src/manual"
